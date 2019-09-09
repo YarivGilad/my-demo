765b29a060e735f24eed8bef156283e53edd112d
Yariv Gilad <yariv.gilad@gmail.com>
2019-09-09 10:28:28 +0300

    added test code

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log('hello git')
+
+const sum = (a,b)=> a+b;
+module.exports = {sum}
--- index.spec.js
+++ index.spec.js
@@ -1,1 +1,7 @@
 //some tests here...
+const {sum} = require('./index');
+
+test('sum 2 numbers',()=>{
+    const result = sum(2,3);
+    expect(result).toBe(5)
+})
